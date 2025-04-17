ef2b19d8c56823ffbb9e1e08a64091e11cd6fd1e
Kuntulu Ankita <kuntuluankita@gmail.com>
2025-04-17 21:15:18 +0530

    Initial Commit

---
+++ ImageViewerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageViewerApp
+//
+//  Created by Kuntulu Ankita on 17/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageViewerApp/ImageViewerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewerAppApp.swift
+//  ImageViewerApp
+//
+//  Created by Kuntulu Ankita on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
